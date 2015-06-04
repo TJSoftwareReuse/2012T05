ea742ca47f3f7c18bce62aced00f31dc3f263293
MarK <emp.yangchunyu@gmail.com>
2015-06-04 16:19:18 +0800

    upload server V5.0

--- 应用程序开发/Server/SoftwareReuseServe_V5.0/src/main/java/Main.java
+++ 应用程序开发/Server/SoftwareReuseServe_V5.0/src/main/java/Main.java
@@ -1,7 +1,7 @@
-import cm.PropertiesUtil;
 import com.team8.PerformanceManagement.PM;
 import org.apache.log4j.Logger;
 import src.com.team8.License.License;
+import tj.reuse.ConfigComponent;
 import warning.DealWarning;
 
 import java.io.IOException;
@@ -20,19 +20,21 @@ public class Main {
         final String RequestQueueName = "RequestQueue";
         //CM.properties route
         final String CMPath = "src/file/project.properties";
-        //license count
-        final int licenseCount = 4;
         //create CM
-        final PropertiesUtil propertiesUtil = new PropertiesUtil(CMPath);
+        final ConfigComponent propertiesUtil = new ConfigComponent();
+        //license count
+        final int licenseCount = Integer.parseInt(propertiesUtil.readValue(CMPath, "License"));
         //get FM_msg route
-        final String FMPath = propertiesUtil.readValue("FM");
+        final String FMPath = propertiesUtil.readValue(CMPath, "FM");
         //get PM_msg route
-        final String PMPath = propertiesUtil.readValue("PM");
+        final String PMPath = propertiesUtil.readValue(CMPath, "PM");
+        //get PM_period
+        final int pmPeriod = Integer.parseInt(propertiesUtil.readValue(CMPath, "PMPeriod"));
 
         //create PM
         PM.setPathName(PMPath);
         //set PM period
-        PM.setPeriod(1);
+        PM.setPeriod(pmPeriod);
         //create FM
         DealWarning dealWarning = new DealWarning(FMPath);
         //create license
@@ -47,28 +49,37 @@ public class Main {
         while (true) {
             String requestMsg = serverGetRequestCustomer.getMsg(); //receive request message from client
             //PM receive msg
-            PM.setPathName(propertiesUtil.readValue("PM"));
+            PM.setPathName(propertiesUtil.readValue(CMPath, "PM"));
             PM.sendPMMessage("ReceiveMessage", 1);
             logger.debug(requestMsg + "/////////////");
+
+            //if the license count has changed
+            int currentLicense = Integer.parseInt(propertiesUtil.readValue(CMPath,"License"));
+
+            if (currentLicense != licenseCount){
+                //license count has changed
+                serverLicense = new License(currentLicense);
+            }
+
             if (serverLicense.inLicense()) {
                 logger.debug("Have license //////////");
                 //get license success
                 //add success FM message
-                dealWarning.setOutoutPath(propertiesUtil.readValue("FM"));
+                dealWarning.setOutoutPath(propertiesUtil.readValue(CMPath,"FM"));
                 dealWarning.OutputInfo("Provide Service");
                 //add PM provider service msg
-                PM.setPathName(propertiesUtil.readValue("PM"));
+                PM.setPathName(propertiesUtil.readValue(CMPath,"PM"));
                 PM.sendPMMessage("ProvideService", 1);
                 //create thread of search model
 
                 //switch the request type
-                if(propertiesUtil.readValue("RequestType").equals("name")){
+                if(propertiesUtil.readValue(CMPath,"RequestType").equals("name")){
                     //query by name
                     MyserverForName myserver = new MyserverForName(requestMsg);
                     Thread t = new Thread(myserver);
                     t.start();
                     logger.debug("MyserverForName create ///////");
-                }else if(propertiesUtil.readValue("RequestType").equals("team")){
+                }else if(propertiesUtil.readValue(CMPath,"RequestType").equals("team")){
                     //query by teamNum
                     MyserverForTeam server = new MyserverForTeam(requestMsg);
                     Thread t = new Thread(server);
@@ -86,13 +97,13 @@ public class Main {
                 errorProducer.connClose();
                 //add failed FM message
                 logger.debug("Don't have license /////////");
-                dealWarning.setOutoutPath(propertiesUtil.readValue("FM"));
+                dealWarning.setOutoutPath(propertiesUtil.readValue(CMPath,"FM"));
                 dealWarning.OutputInfo("Reject Service");
                 //add PM reject service msg
-                PM.setPathName(propertiesUtil.readValue("PM"));
+                PM.setPathName(propertiesUtil.readValue(CMPath,"PM"));
                 PM.sendPMMessage("RejectService", 1);
                 //add PM return message msg
-                PM.setPathName(propertiesUtil.readValue("PM"));
+                PM.setPathName(propertiesUtil.readValue(CMPath,"PM"));
                 PM.sendPMMessage("ReturnMessage", 1);
             }
         }
